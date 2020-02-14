410237c7705e756c8526d579037e4f85a091fed9
Michael Crews <crews@mines.edu>
2020-02-13 21:49:02 -0700

    Added new text and changed the cat's mouth to black

--- src/Cat.java
+++ src/Cat.java
@@ -14,7 +14,7 @@ public class Cat {
 	private static final int EYE_HEIGHT = 10;
 	private static final int EYE_WIDTH = 10;
 	// pick mouth height, width is based on head dimension
-	private static final int MOUTH_HEIGHT = 10;
+	private static final int MOUTH_HEIGHT = 70;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
@@ -37,13 +37,13 @@ public class Cat {
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.pink);
+		g2.setColor(Color.black);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Mew", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("Mew, I'm dead inside", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
